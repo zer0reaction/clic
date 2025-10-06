75777ea2b41a5d6de9640d845d4632864ab0e0e2
zer0reaction <boltgopnik@gmail.com>
2025-10-07 00:27:32 +0300

    Move type checking to typecheck.go

--- parser/parser.go
+++ parser/parser.go
@@ -52,37 +52,6 @@ func (p *Parser) CreateAST() *Node {
 	return head
 }
 
-func (p *Parser) TypeCheck(n *Node) {
-	if n == nil {
-		return
-	}
-
-	switch n.Tag {
-	case NodeBinOp:
-		p.checkBinOp(n)
-	case NodeInteger:
-		// do nothing
-	case NodeBoolean:
-		// do nothing
-	case NodeBlock:
-		p.TypeCheck(n.Block.Start)
-	case NodeVariableDecl:
-		// do nothing
-	case NodeVariable:
-		// do nothing
-	case NodeFunEx:
-		// do nothing
-	case NodeFunCall:
-		p.TypeCheck(n.Function.ArgStart)
-	case NodeIf:
-		p.checkIf(n)
-	default:
-		panic("node not implemented")
-	}
-
-	p.TypeCheck(n.Next)
-}
-
 func (p *Parser) reportHere(n *Node, tag report.ReportTag, msg string) {
 	report.Report(report.Form{
 		Tag:    tag,
@@ -291,44 +260,4 @@ func (p *Parser) parseItem() *Node {
 	}
 
 	return &n
-}
-
-func (p *Parser) checkBinOp(n *Node) {
-	if n.Tag != NodeBinOp {
-		panic("incorrect node tag")
-	}
-
-	p.TypeCheck(n.BinOp.Lval)
-	p.TypeCheck(n.BinOp.Rval)
-
-	lvalType := n.BinOp.Lval.GetType()
-	rvalType := n.BinOp.Rval.GetType()
-	if lvalType != rvalType {
-		p.reportHere(n,
-			report.ReportNonfatal,
-			"operand type mismatch")
-	}
-
-	isAssign := (n.BinOp.Tag == BinOpAssign)
-	isStorage := (n.BinOp.Lval.Tag == NodeVariable)
-	if isAssign && !isStorage {
-		p.reportHere(n,
-			report.ReportNonfatal,
-			"lvalue is not a storage location")
-	}
-}
-
-func (p *Parser) checkIf(n *Node) {
-	if n.Tag != NodeIf {
-		panic("incorrect node tag")
-	}
-
-	expType := n.If.Exp.GetType()
-	if expType != types.Bool {
-		p.reportHere(n,
-			report.ReportNonfatal,
-			"expected boolean type in expression")
-	}
-
-	p.TypeCheck(n.If.Body)
 }
