9eb4f7a741a713aeef96c4f8d266e4de2cd99698
zer0reaction <boltgopnik@gmail.com>
2025-10-19 14:01:41 +0300

    Remove test files

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $# -ne 1 ]]; then
-    echo "You must specify the name of the test file"
-    exit 1
-fi
-
-go run ./cmd/main.go -bf "-ggdb -o /tmp/out ./extern.c" $1 && /tmp/out
